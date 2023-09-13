ccdd4a054026976247e169c2ee23a117af206df2
李魁 <melikui@qq.com>
2023-09-13 09:36:13 +0800

    init

---
+++ src/main/java/com/today/app/web/WebController.java
@@ -0,0 +1,20 @@
+package com.today.app.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class WebController {
+    @GetMapping ("/getDemo")
+    public String getDemo() {
+        return "666 Hello @ Spring Boot! ";
+    }
+
+    @PostMapping ("/postDemo")
+    public String postDemo() {
+        return "Post 请求";
+    }
+}
