112e05c3c21c0857f01227294ed8cc37e69feaf3
李魁 <melikui@qq.com>
2023-09-16 11:16:54 +0800

    优化

--- src/test/java/com/today/App/ApplicationTests.java
+++ src/test/java/com/today/App/ApplicationTests.java
@@ -10,45 +10,15 @@ import java.util.List;
 
 @SpringBootTest
 class ApplicationTests {
-
     @Autowired
     private StudentMapper studentMapper;
 
     @Test
-    public void testInsert() {
-        // 新增
-        studentMapper.insert(new Student("weiz新增", 1, 30));
-    }
-
-//    @Test
-//    public void testUpdate() {
-//        Student student = studentMapper.selectOne(4L);
-//        student.setName("weiz修改");
-//        student.setSex(0);
-//        // 修改
-//        studentMapper.update(student);
-//    }
-
-    @Test
-    public void testSelectOne() {
-        // 查询
-        Student student = studentMapper.selectOne(13L);
-        System.out.println("name:" + student.getName() + ",age:" + student.getAge());
-    }
-
-    //
-//    @Test
-//    public void testDelete() {
-//        // 删除
-//        studentMapper.delete(4L);
-//    }
-    @Test
     public void testSelectAll() {
-        // 查询
+        // 查询全部
         List<Student> students = studentMapper.selectAll();
         for (Student stu : students) {
             System.out.println("name:" + stu.getName() + ",age:" + stu.getAge());
         }
     }
-
 }
