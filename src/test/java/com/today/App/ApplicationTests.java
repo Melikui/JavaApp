428acca765b91e482af5b6f6c5ba15144dcdbe00
李魁 <melikui@qq.com>
2023-10-07 09:44:42 +0800

    优化

--- src/test/java/com/today/App/ApplicationTests.java
+++ src/test/java/com/today/App/ApplicationTests.java
@@ -1,12 +1,16 @@
 package com.today.App;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import java.util.HashMap;
+
 @SpringBootTest
+@Slf4j
 class ApplicationTests {
     @Autowired
     private RedisTemplate<String, Object> redisTemplate;
@@ -16,6 +20,20 @@ class ApplicationTests {
         redisTemplate.opsForValue().set("username", "melikui");
         Object result = redisTemplate.opsForValue().get("username");
         System.out.println(result);
+    }
+
+    @Test
+    public void dataHash() {
+        // 1、写数据
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("key1", "value1");
+        hashMap.put("key2", "value2");
+        redisTemplate.opsForHash().putAll("data-hash", hashMap);
+        // 2、读数据
+        Object kv1 = redisTemplate.opsForHash().get("data-hash", "key1");
+        Object kv2 = redisTemplate.opsForHash().get("data-hash", "key2");
+        System.out.println("kv1---" + kv1);
+        System.out.println("kv2---" + kv2);
     }
 }
 
