4d4e82710f566cbb167f414e54c4982432e90d9a
李魁 <melikui@qq.com>
2023-10-08 15:34:22 +0800

    优化

--- src/main/java/com/today/base/example/HttpClientsDemo.java
+++ src/main/java/com/today/base/example/HttpClientsDemo.java
@@ -1,24 +1,26 @@
 package com.today.base.example;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 
-public class HttpClients {
+public class HttpClientsDemo {
     public static void getHtml() throws IOException {
         // 创建HttpClient对象
-        CloseableHttpClient httpClient = org.apache.http.impl.client.HttpClients.createDefault();
+        CloseableHttpClient httpClient = HttpClients.createDefault();
         // 创建GET请求
-        HttpGet httpGet = new HttpGet("https://blog.csdn.net/weixin_43347659");
-        httpGet.setHeader("use-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36");
+        HttpGet httpGet = new HttpGet("https://api.github.com/search/users?q=likui");
         // 获取响应结果
         CloseableHttpResponse response = httpClient.execute(httpGet);
         if (response.getStatusLine().getStatusCode() == 200) {
-            String html = EntityUtils.toString(response.getEntity(), "UTF-8");
-            System.out.println(html);
+            HttpEntity entity = response.getEntity();
+            String responseJson = EntityUtils.toString(entity, "UTF-8");
+            System.out.println(responseJson);
         }
         httpClient.close();
         response.close();
