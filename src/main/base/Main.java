569692b4dee0f93eaf88f0e2e41d4257ee0929c0
李魁 <melikui@qq.com>
2023-10-09 13:34:25 +0800

    优化

--- src/main/base/Main.java
+++ src/main/base/Main.java
@@ -1,9 +1,14 @@
 import util.FileUtil;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
         String path = "/Users/likui/Desktop/Java/file/text/bilibili.txt";
-        String content = FileUtil.fileRead(path);
-        System.out.println(content);
+//        String content = FileUtil.fileRead(path);
+//        System.out.println(content);
+        File file = new File(path);
+        System.out.println(file.exists());
+        System.out.println(file.getParent());
     }
 }
