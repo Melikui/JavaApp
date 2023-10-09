c86357e45601280f1ccbc97d6b210a06b3d6721f
李魁 <melikui@qq.com>
2023-10-09 14:40:41 +0800

    优化

--- src/main/base/Main.java
+++ src/main/base/Main.java
@@ -1,8 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        int x = 10, y = 20;
-        String html = """ 
-                """;
+        System.out.println(666);
     }
 }
