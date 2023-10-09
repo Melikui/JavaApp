d14bd99eb748f287be64ac279736870e011a40d1
李魁 <melikui@qq.com>
2023-10-09 14:24:42 +0800

    优化

--- src/main/base/Main.java
+++ src/main/base/Main.java
@@ -1,10 +1,6 @@
-import plugins.MySQL;
-
-import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
-        MySQL mysql = new MySQL();
-        mysql.dbQuery();
+    public static void main(String[] args) {
+
     }
 }
---
+++ src/main/base/example/Gougudingli.java
@@ -0,0 +1,15 @@
+package example;
+
+public class Gougudingli {
+    public static void main(String[] args) {
+        for (int x = 1; x <= 100; x++) {
+            for (int y = 1; y <= 100; y++) {
+                for (int z = 1; z <= 100; z++) {
+                    if (x < y && x * x + y * y == z * z) {
+                        System.out.println(x + "," + y + "," + z);
+                    }
+                }
+            }
+        }
+    }
+}
