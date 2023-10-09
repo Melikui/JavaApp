2df0e3ed9cb5856ddb3946c2d5d8d54760c219ce
李魁 <melikui@qq.com>
2023-10-09 10:43:01 +0800

    优化

--- src/main/java/com/today/base/Main.java
+++ src/main/java/com/today/base/Main.java
@@ -1,28 +1,10 @@
 package com.today.base;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 
-import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        // 创建HttpClient对象
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        // 创建GET请求
-        HttpGet httpGet = new HttpGet("https://api.github.com/search/users?q=likui");
-        // 获取响应结果
-        CloseableHttpResponse response = httpClient.execute(httpGet);
-        if (response.getStatusLine().getStatusCode() == 200) {
-            HttpEntity entity = response.getEntity();
-            String responseJson = EntityUtils.toString(entity, "UTF-8");
-            System.out.println(responseJson);
-        }
-        httpClient.close();
-        response.close();
+    public static void main(String[] args) {
+
+
     }
 }
---
+++ src/main/java/com/today/base/example/Terminal.java
@@ -0,0 +1,26 @@
+package com.today.base.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class Terminal {
+    public static void run() throws IOException, InterruptedException {
+        String[] command = {"ls", "-a"};
+        Process process = Runtime.getRuntime().exec(command);
+        // 读取命令执行结果
+        InputStream inputStream = process.getInputStream();
+        System.out.println(inputStream);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+        // 等待命令执行完毕
+        process.waitFor();
+        // 关闭资源
+        reader.close();
+        inputStream.close();
+    }
+}
