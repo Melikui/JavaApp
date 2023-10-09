e2259232fb574b74b33fa5173c00ae2bed09c7bb
李魁 <melikui@qq.com>
2023-10-09 12:24:28 +0800

    优化

--- src/main/java/com/today/base/algorithm/Search.java
+++ src/main/java/com/today/base/algorithm/Search.java
@@ -49,15 +49,15 @@ public class Search {
      * @return boolean
      */
     public boolean binarySearch(int[] list, int item) {
-        //1.定义两个变量记录要查找的范围
+        // 1.定义两个变量记录要查找的范围
         int first = 0;
         int last = list.length - 1;
         boolean found = false;
-        //2.利用循环不断的去找要查找的数据
+        // 2.利用循环不断的去找要查找的数据
         while (first <= last && !found) {
-            //3.找到min和max的中间位置
+            // 3.找到min和max的中间位置
             int mid = (first + last) / 2;
-            //4.拿着mid指向的元素跟要查找的元素进行比较
+            // 4.拿着mid指向的元素跟要查找的元素进行比较
             if (list[mid] == item) {
                 return true;
             } else {
