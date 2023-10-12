b86450325b60389ddb76d40d98909c887ac794f6
李魁 <melikui@qq.com>
2023-10-12 22:34:29 +0800

    Life is long, you need to lean Java ~

--- src/main/base/Main.java
+++ src/main/base/Main.java
@@ -1,9 +1,7 @@
 
-import algorithm.Search;
-
 public class Main {
-    public static void main(String[] args)  {
-        System.out.println(Search.binarySearch(new int[]{1, 3, 5}, 3));
+    public static void main(String[] args) {
+        System.out.println(666);
     }
 }
 
