432b5a82bc9f9ad585611931f48bdfaf2c35ecc8
李魁 <melikui@qq.com>
2023-10-13 23:32:36 +0800

    Life is long, you need to lean Java ~

--- src/main/base/Main.java
+++ src/main/base/Main.java
@@ -1,11 +1,11 @@
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.util.Arrays;
 
 import static algorithm.Search.binarySearch;
 
 public class Main {
     public static void main(String[] args) {
         int[] list = {1, 6, 3, 63, 32, 34, 43, 35};
+        Arrays.sort(list);
         System.out.println(binarySearch(list, 43));
     }
 
