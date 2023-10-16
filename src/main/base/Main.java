862804dbac99d845894b9ba01fd53fe591456631
李魁 <melikui@qq.com>
2023-10-16 15:10:58 +0800

    优化

--- src/main/base/Main.java
+++ src/main/base/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Jwt jwt = new Jwt();
         String token = jwt.generateToken();
-        jwt.verifyToken(token);;
+        jwt.verifyToken(token);
     }
 }
