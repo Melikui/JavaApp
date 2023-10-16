3602c31658c8038e96b9508dd641d658a903b419
李魁 <melikui@qq.com>
2023-10-16 14:49:42 +0800

    优化

--- src/main/base/Main.java
+++ src/main/base/Main.java
@@ -1,12 +1,6 @@
-import algorithm.Sort;
-
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1, 32, 6, 31, 64, 23, 13, 9};
-        Sort.quickSort(arr, 0, arr.length - 1);
-        System.out.println(Arrays.toString(arr));
+
     }
 }
-
--- src/main/base/plugins/MongoDB.java
+++ src/main/base/plugins/MongoDB.java
@@ -1,5 +1,45 @@
 package plugins;
 
-public class MongoDB {
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
+
+public class MongoDB {
+    public static void main(String[] args) {
+        /*
+         * https://zhuanlan.zhihu.com/p/622185505
+         */
+        try {
+            // 连接到 mongodb 服务
+            String url = "mongodb://127.0.0.1:27017";
+            MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                    .applyConnectionString(new ConnectionString(url))
+                    .build();
+            com.mongodb.client.MongoClient mongoClient = MongoClients.create(mongoClientSettings);
+            // 连接到数据库
+            MongoDatabase mongoDatabase = mongoClient.getDatabase("spider");
+            System.out.println("Connect to database successfully");
+
+            MongoCollection<Document> collection = mongoDatabase.getCollection("user");
+            System.out.println("集合 test 选择成功");
+            /*
+              检索所有文档
+              1. 获取迭代器FindIterable<Document>
+              2. 获取游标MongoCursor<Document>
+              3. 通过游标遍历检索出的文档集合
+             */
+            FindIterable<Document> findIterable = collection.find();
+            for (Document document : findIterable) {
+                System.out.println(document);
+            }
+            mongoClient.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+    }
 }
