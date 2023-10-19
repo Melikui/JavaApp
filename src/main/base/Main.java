eafbc486221e0df7ec0daf8e8751a3dcd2d14145
李魁 <melikui@qq.com>
2023-10-19 11:32:10 +0800

    优化

--- src/main/base/Main.java
+++ src/main/base/Main.java
@@ -1,11 +1,20 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 10000; i++) {
-            sb.append(i);
+        // 创建集合
+        ArrayList<String> sites = new ArrayList<>();
+        sites.add("Google");
+        sites.add("Runoob");
+        sites.add("Taobao");
+        sites.add("Zhihu");
+        // 获取迭代器
+        Iterator<String> it = sites.iterator();
+        // 输出集合中的第一个元素
+        System.out.println("-----");
+        while (it.hasNext()){
+            System.out.println(it.next());
         }
-        System.out.println(sb);
     }
 }
