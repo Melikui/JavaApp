dbea15f28cab4408757e800fb58a0f4712a5f5d4
李魁 <melikui@qq.com>
2023-10-20 09:14:39 +0800

    1

--- src/main/java/com/today/App/security/MyFilter.java
+++ src/main/java/com/today/App/security/MyFilter.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-//@Component
+@Component
 @Slf4j
 @WebFilter(urlPatterns = {"/*"})
 public class MyFilter implements Filter {
