137a3fae1455e1fb64ac963076ed260bacf22282
李魁 <melikui@qq.com>
2023-10-20 12:26:12 +0800

    1

--- src/main/base/example/GithubUser.java
+++ src/main/base/example/GithubUser.java
@@ -1,16 +1,20 @@
 package example;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.apache.tomcat.util.json.JSONParser;
+import org.apache.tomcat.util.json.ParseException;
 
 import java.io.IOException;
 
-public class HttpClientsDemo {
-    public static void main(String[] args) throws IOException {
+public class GithubUser {
+    public static void main(String[] args) throws IOException, ParseException {
         // 创建HttpClient对象
         CloseableHttpClient httpClient = HttpClients.createDefault();
         // 创建GET请求
@@ -20,7 +24,11 @@ public class HttpClientsDemo {
         if (response.getStatusLine().getStatusCode() == 200) {
             HttpEntity entity = response.getEntity();
             String responseJson = EntityUtils.toString(entity, "UTF-8");
-            System.out.println(responseJson);
+            JSONArray users = JSONObject.parseObject(responseJson).getJSONArray("items");
+            for (Object user : users) {
+                System.out.println(new JSONParser(user.toString()).parseObject().get("avatar_url"));
+                System.out.println(JSONObject.parseObject(user.toString()).get("gists_url"));
+            }
         }
         httpClient.close();
         response.close();
