269d694a32d7e2f6e0f15aaaf8239541e80b6034
李魁 <melikui@qq.com>
2023-10-21 16:07:08 +0800

    1

--- src/main/base/Main.java
+++ src/main/base/Main.java
@@ -1,14 +1,10 @@
-import util.FileUtil;
+import plugins.JavaSocket;
+
 import java.io.IOException;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String path = "/Users/likui/Desktop/Java/file/text/foo.txt";
-        FileUtil.bufferedWrite(path, """
-                Java 是一个非常好的语言。
-                是的，的确非常好!!
-                """,true);
-        System.out.println(FileUtil.fileRead(path));
+        JavaSocket.Client();
     }
 }
---
+++ src/main/base/plugins/JavaSocket.java
@@ -0,0 +1,31 @@
+package plugins;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class JavaSocket {
+    public static void Client() throws IOException {
+        String msg = "";
+        while (!Objects.equals(msg, "stop")) {
+            Socket socket = new Socket("localhost", 8880);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("请输入发送的内容: ");
+            msg = scanner.next();
+            out.println(msg);
+            String response = br.readLine();
+            System.out.println("---Python: " + response);
+            out.close();
+            br.close();
+            socket.close();
+        }
+    }
+
+    public static void Server() {
+
+    }
+}
+
