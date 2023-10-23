3c91ce66f0dff87a6b2312e21709fb6c4b841a01
李魁 <melikui@qq.com>
2023-10-23 16:39:13 +0800

    1

--- src/main/base/Main.java
+++ src/main/base/Main.java
@@ -1,12 +1,8 @@
 import lombok.extern.slf4j.Slf4j;
-import util.ExcelUtil;
-
-import java.io.IOException;
 
 @Slf4j
 public class Main {
-    public static void main(String[] args) throws IOException {
-        // log.info("日志记录: {}", numbers);
-        ExcelUtil.writeExcel();
+    public static void main(String[] args) {
+        // log.info("日志记录: {}", 666);
     }
 }
