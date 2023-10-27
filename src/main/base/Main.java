8018153173a2e8c617cf9e3395d73fd9aaddbf39
李魁 <melikui@qq.com>
2023-10-27 10:29:49 +0800

    优化

--- src/main/base/Main.java
+++ src/main/base/Main.java
@@ -1,9 +1,13 @@
+import java.io.File;
+
 public class Main {
     // -----------------------------------
     public static void main(String[] args) {
-
+        String str = "Hello world ,Hello Runoob";
+        int index = str.lastIndexOf("o");
+        System.out.println("index： " + index);
+        System.out.println(str.replace("H","K"));
+        System.out.println(new StringBuffer(str).reverse().toString());
     }
     // -----------------------------------
-
-
 }
