3674ce45fde95ba30e4726aac78dd9c1622d4a14
李魁 <melikui@qq.com>
2023-10-27 13:58:10 +0800

    优化

--- src/main/base/Main.java
+++ src/main/base/Main.java
@@ -1,18 +1,11 @@
-import java.io.FileInputStream;
 import java.io.IOException;
+
+import static example.GithubUser.getGithubUser;
 
 public class Main {
     // -----------------------------------
     public static void main(String[] args) throws IOException {
-        StringBuilder content = new StringBuilder();
-        FileInputStream fis = new FileInputStream("file/mysql/mysql.sql");
-        byte[] bytes = new byte[1024];
-        int len;
-        while ((len = fis.read(bytes)) != -1) {
-            content.append(new String(bytes, 0, len));
-        }
-        System.out.println(content);
-        fis.close();
+        getGithubUser();
     }
     // -----------------------------------
 }
--- src/main/base/example/GithubUser.java
+++ src/main/base/example/GithubUser.java
@@ -6,14 +6,13 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-import org.apache.tomcat.util.json.ParseException;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
 
 public class GithubUser {
-    public static void main(String[] args) throws IOException, ParseException {
+    public static void getGithubUser() throws IOException {
         // 创建HttpClient对象
         CloseableHttpClient httpClient = HttpClients.createDefault();
         // 创建GET请求
@@ -23,10 +22,10 @@ public class GithubUser {
         if (response.getStatusLine().getStatusCode() == 200) {
             HttpEntity entity = response.getEntity();
             String responseJson = EntityUtils.toString(entity, "UTF-8");
-            org.json.JSONArray users =(JSONArray) ((new org.json.JSONObject(responseJson)).get("items"));
+            JSONArray users =(JSONArray) ((new JSONObject(responseJson)).get("items"));
             System.out.println(users);
             for (Object user : users) {
-                org.json.JSONObject userMap = (JSONObject)user;
+                JSONObject userMap = (JSONObject)user;
                 System.out.println(userMap.get("login"));
                 System.out.println(userMap.get("url"));
                 System.out.println(userMap.get("avatar_url"));
