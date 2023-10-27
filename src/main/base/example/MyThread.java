5f3379b6c8922dd071f1792babc8dacb655122cf
Melikui <melikui@qq.com>
2023-10-27 22:55:34 +0800

    优化

--- src/main/base/example/MyThread.java
+++ src/main/base/example/MyThread.java
@@ -12,6 +12,11 @@ public class MyThread extends Thread {
     public void run(){
         // 线程要执行的代码
         for (int i = 0; i < 100; i++) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
             System.out.println(getName()+" --- Hello World");
         }
     }
