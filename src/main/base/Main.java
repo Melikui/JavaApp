bf8fc4f859eaa38bce5442ccb20de9941f7a8614
李魁 <melikui@qq.com>
2023-10-29 21:09:55 +0800

    优化

--- src/main/base/Main.java
+++ src/main/base/Main.java
@@ -1,11 +1,13 @@
-import java.util.Arrays;
+import java.util.UUID;
 
 public class Main {
     // -----------------------------------
     public static void main(String[] args) {
-        int[] arr = {1, 9, 6, 5, 2, 3, 4, 8, 7, 10, 0};
-        shellSort(arr);
-        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < 10; i++) {
+            String uuid = UUID.randomUUID().toString();
+            uuid = uuid.replace("-", "");
+            System.out.println(uuid);
+        }
     }
 
     // -----------------------------------
