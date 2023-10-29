9a4c1b7ffb6b5fce8d108c3c324909419190f540
李魁 <melikui@qq.com>
2023-10-29 20:49:52 +0800

    优化

--- src/main/base/astudy/network/TCP/Client.java
+++ src/main/base/astudy/network/TCP/Client.java
@@ -3,19 +3,26 @@ package astudy.network.TCP;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws IOException {
         // TCP 协议，发送数据
-        // 1,创建 Socket 对象
-        // 细节：在创建对象的同时会连接服务器，如果连接不上，代码会报错
+        // 1,创建 Socket 对象并连接服务器（如果连接不上，代码会报错）
         Socket socket = new Socket("127.0.0.1", 10000);
         // 2,可以从连接通道中获取输出流
+        Scanner scanner = new Scanner(System.in);
         OutputStream os = socket.getOutputStream();
-        // 写出数据
-        os.write("Hello World, Java 是一门非常优秀的语言。 ".getBytes());
+        while (true) {
+            System.out.print("请输入您要发送的信息: ");
+            String msg = scanner.nextLine();
+            if ("end".equals(msg)) {
+                break;
+            }
+            // 写出数据
+            os.write(msg.getBytes());
+        }
         // 3,释放资源
-        os.close();
         socket.close();
     }
 }
