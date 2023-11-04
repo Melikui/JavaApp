97e60c3893803500130769d4a65739f3e4027b9d
李魁 <melikui@qq.com>
2023-11-04 16:04:21 +0800

    优化

--- src/main/java/com/today/App/config/SwaggerConfig.java
+++ src/main/java/com/today/App/config/SwaggerConfig.java
@@ -6,6 +6,9 @@ import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * url: http://127.0.0.1:8080/swagger-ui/index.html
+ */
 @Configuration
 public class SwaggerConfig {
     @Bean
--- src/main/java/com/today/App/config/WebMvcConfig.java
+++ src/main/java/com/today/App/config/WebMvcConfig.java
@@ -1,13 +1,19 @@
 package com.today.App.config;
 
 import com.today.App.security.AuthInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfig extends WebMvcConfigurationSupport {
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Bean(name = "authInterceptor")
+    public AuthInterceptor authInterceptor() {
+        return new AuthInterceptor();
+    }
     /*
      * 拦截器配置
      */
@@ -16,15 +22,17 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         // 注册自定义拦截器，添加拦截路径和排除拦截路径
         registry.addInterceptor(new AuthInterceptor()) // 添加拦截器
                 .addPathPatterns("/**") // 添加拦截路径
-                .excludePathPatterns("/login.html", "/static/**"); // 添加排除拦截路径及执行顺序
-        super.addInterceptors(registry);
+                .excludePathPatterns("/login.html", "/static/**") // 添加排除拦截路径及执行顺序
+                .excludePathPatterns("/swagger-resources/**", "/swagger-ui/**", "/v3/**", "/error");
     }
+
     /**
      * 添加静态资源文件，外部可以直接访问地址
      */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
         //此处还可继续增加目录。。。。
     }
 }
