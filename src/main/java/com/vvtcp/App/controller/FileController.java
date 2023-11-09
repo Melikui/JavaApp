fb54ee25ef9ad126f66e37565885e58577fe799c
李魁 <melikui@qq.com>
2023-11-09 10:55:19 +0800

    优化

--- src/main/java/com/vvtcp/App/controller/FileController.java
+++ src/main/java/com/vvtcp/App/controller/FileController.java
@@ -1,5 +1,6 @@
 package com.vvtcp.App.controller;
 
+import com.vvtcp.App.util.JSONResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -17,7 +18,7 @@ public class FileController {
      * 文件上传
      */
     @PostMapping("/upload")
-    public void upload(
+    public JSONResult upload(
             @RequestPart("file") MultipartFile file,
             @RequestPart("image") MultipartFile image,
             @RequestPart("photos") MultipartFile[] photos
@@ -28,14 +29,15 @@ public class FileController {
         }
         if (!image.isEmpty()) {
             String imgName = image.getOriginalFilename();
-            image.transferTo(new File("/Users/likui/Desktop/Java/file/image/" + imgName));
+            image.transferTo(new File("/Users/likui/Desktop/Java/file/" + imgName));
         }
         for (MultipartFile photo : photos) {
             if (!photo.isEmpty()) {
                 String imgName = photo.getOriginalFilename();
-                photo.transferTo(new File("/Users/likui/Desktop/Java/file/image/" + imgName));
+                photo.transferTo(new File("/Users/likui/Desktop/Java/file/" + imgName));
             }
         }
+        return JSONResult.ok("上传成功!");
     }
     @PostMapping("/download")
     public void download() {
